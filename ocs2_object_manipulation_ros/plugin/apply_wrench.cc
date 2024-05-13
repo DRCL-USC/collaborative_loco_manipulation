ccd023e1ccb9c0c768e2dfd6d3028219bf2e4e49
mohsen <somboles@usc.edu>
2024-05-13 11:37:58 -0700

    change method of applying wrench

---
+++ ocs2_object_manipulation_ros/plugin/apply_wrench.cc
@@ -0,0 +1,197 @@
+
+#include <algorithm>
+#include <assert.h>
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+#include <geometry_msgs/Wrench.h>
+#include <geometry_msgs/Pose.h>
+
+#include <ros/ros.h>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+
+#include <gazebo/physics/physics.hh>
+#include <gazebo/transport/TransportTypes.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/Events.hh>
+
+namespace gazebo
+{
+
+    class ApplyBodyForce : public ModelPlugin
+    {
+    public:
+        // Constructor
+        ApplyBodyForce()
+        {
+            this->wrench_msg_.force.x = 0;
+            this->wrench_msg_.force.y = 0;
+            this->wrench_msg_.force.z = 0;
+            this->wrench_msg_.torque.x = 0;
+            this->wrench_msg_.torque.y = 0;
+            this->wrench_msg_.torque.z = 0;
+            this->pose_msg_.position.x = 0;
+            this->pose_msg_.position.y = 0;
+            this->pose_msg_.position.z = 0;
+        }
+
+        // Destructor
+        ~ApplyBodyForce()
+        {
+            this->update_connection_.reset();
+
+            // Custom Callback Queue
+            this->queue_[0].clear();
+            this->queue_[0].disable();
+            this->queue_[1].clear();
+            this->queue_[1].disable();
+            this->rosnode_->shutdown();
+            this->callback_queue_thread_.join();
+
+            delete this->rosnode_;
+        }
+
+    protected:
+        // Load the controller
+        void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+        {
+            // Get the world name.
+            this->world_ = _model->GetWorld();
+
+            // load parameters
+            this->robot_namespace_ = "";
+            if (_sdf->HasElement("robotNamespace"))
+                this->robot_namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>() + "/";
+
+            if (!_sdf->HasElement("bodyName"))
+            {
+                ROS_FATAL_NAMED("force", "force plugin missing <bodyName>, cannot proceed");
+                return;
+            }
+            else
+                this->link_name_ = _sdf->GetElement("bodyName")->Get<std::string>();
+
+            this->link_ = _model->GetLink(this->link_name_);
+            if (!this->link_)
+            {
+                ROS_FATAL_NAMED("force", "gazebo_ros_force plugin error: link named: %s does not exist\n", this->link_name_.c_str());
+                return;
+            }
+
+            if (!_sdf->HasElement("ForceTopicName"))
+            {
+                ROS_FATAL_NAMED("force", "force plugin missing <ForceTopicName>, cannot proceed");
+                return;
+            }
+            else
+                this->force_topic_name_ = _sdf->GetElement("ForceTopicName")->Get<std::string>();
+
+            if (!_sdf->HasElement("referencePointTopicName"))
+            {
+                ROS_FATAL_NAMED("force", "force plugin missing <referencePointTopicName>, cannot proceed");
+                return;
+            }
+            else
+                this->reference_point_topic_name_ = _sdf->GetElement("referencePointTopicName")->Get<std::string>();
+
+            // Make sure the ROS node for Gazebo has already been initialized
+            if (!ros::isInitialized())
+            {
+                ROS_FATAL_STREAM_NAMED("force", "A ROS node for Gazebo has not been initialized, unable to load plugin. "
+                                                    << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+                return;
+            }
+
+            this->rosnode_ = new ros::NodeHandle(this->robot_namespace_);
+
+            // Custom Callback Queue
+            ros::SubscribeOptions so = ros::SubscribeOptions::create<geometry_msgs::Wrench>(
+                this->force_topic_name_, 1,
+                boost::bind(&ApplyBodyForce::UpdateObjectForce, this, _1),
+                ros::VoidPtr(), &this->queue_[0]);
+
+            ros::SubscribeOptions fo = ros::SubscribeOptions::create<geometry_msgs::Pose>(
+                this->reference_point_topic_name_, 1,
+                boost::bind(&ApplyBodyForce::UpdateContactPoint, this, _1),
+                ros::VoidPtr(), &this->queue_[1]);
+
+            this->sub_[0] = this->rosnode_->subscribe(so);
+            this->sub_[1] = this->rosnode_->subscribe(fo);
+
+            // Custom Callback Queue
+            this->callback_queue_thread_ = boost::thread(boost::bind(&ApplyBodyForce::QueueThread, this));
+
+            // New Mechanism for Updating every World Cycle
+            // Listen to the update event. This event is broadcast every
+            // simulation iteration.
+            this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+                boost::bind(&ApplyBodyForce::UpdateChild, this));
+        }
+
+        // Update the controller
+        void UpdateObjectForce(const geometry_msgs::Wrench::ConstPtr &_msg)
+        {
+            this->wrench_msg_.force.x = _msg->force.x;
+            this->wrench_msg_.force.y = _msg->force.y;
+            this->wrench_msg_.force.z = _msg->force.z;
+            this->wrench_msg_.torque.x = _msg->torque.x;
+            this->wrench_msg_.torque.y = _msg->torque.y;
+            this->wrench_msg_.torque.z = _msg->torque.z;
+        }
+
+        void UpdateContactPoint(const geometry_msgs::Pose::ConstPtr &_msg)
+        {
+            this->pose_msg_.position.x = _msg->position.x;
+            this->pose_msg_.position.y = _msg->position.y;
+            this->pose_msg_.position.z = _msg->position.z;
+        }
+
+        // Update the controller
+        void UpdateChild()
+        {
+            this->lock_.lock();
+            ignition::math::Vector3d force(this->wrench_msg_.force.x, this->wrench_msg_.force.y, this->wrench_msg_.force.z);
+            ignition::math::Vector3d torque(this->wrench_msg_.torque.x, this->wrench_msg_.torque.y, this->wrench_msg_.torque.z);
+            ignition::math::Vector3d pose(this->pose_msg_.position.x, this->pose_msg_.position.y, this->pose_msg_.position.z);
+            // this->link_->AddLinkForce(force, pose);
+            // this->link_->AddRelativeTorque(torque);
+
+            this->link_->AddForce(force);
+            this->link_->AddTorque(torque);
+
+            this->lock_.unlock();
+        }
+
+        // Custom Callback Queue
+        // custom callback queue thread
+        void QueueThread()
+        {
+            static const double timeout = 0.01;
+
+            while (this->rosnode_->ok())
+            {
+                this->queue_[0].callAvailable(ros::WallDuration(timeout));
+                this->queue_[1].callAvailable(ros::WallDuration(timeout));
+            }
+        }
+
+    private:
+        physics::WorldPtr world_;
+        physics::LinkPtr link_;
+        ros::NodeHandle *rosnode_;
+        ros::Subscriber sub_[2];
+        boost::mutex lock_;
+        std::string force_topic_name_;
+        std::string reference_point_topic_name_;
+        std::string link_name_;
+        std::string robot_namespace_;
+        ros::CallbackQueue queue_[2];
+        boost::thread callback_queue_thread_;
+        geometry_msgs::Wrench wrench_msg_;
+        geometry_msgs::Pose pose_msg_;
+        event::ConnectionPtr update_connection_;
+    };
+    // Register this plugin with the simulator
+    GZ_REGISTER_MODEL_PLUGIN(ApplyBodyForce);
+
+}
--- ocs2_object_manipulation_ros/src/ApplyWrenchNode.cpp
+++ ocs2_object_manipulation_ros/src/ApplyWrenchNode.cpp
@@ -13,53 +13,27 @@ int main(int argc, char **argv)
 
     // Initialize the ROS node handle
     ros::NodeHandle nh;
-
-    // Create a client to call the service
-    ros::ServiceClient client = nh.serviceClient<gazebo_msgs::ApplyBodyWrench>("/gazebo/apply_body_wrench");
-
-    // Create a service request object
-    gazebo_msgs::ApplyBodyWrench srv[3];
     
     // Create a wrench vector
     ocs2::vector_t wrench;
+
+    // Create a publish message
+    geometry_msgs::Wrench wrench_msg;
+    ros::Publisher pub = nh.advertise<geometry_msgs::Wrench>("/robot_1/wrench", 1);
 
     auto WrenchCallback = [&](const ocs2_msgs::mpc_observation::ConstPtr &msg)
     {
         std::unique_ptr<ocs2::SystemObservation> observationPtr_(new ocs2::SystemObservation(ocs2::ros_msg_conversions::readObservationMsg(*msg)));
         
         wrench = observationPtr_->input;
+        wrench_msg.force.x = wrench(0);
+        wrench_msg.force.y = wrench(1);
+        wrench_msg.force.z = 0;
+        wrench_msg.torque.x = 0;
+        wrench_msg.torque.y = 0;
+        wrench_msg.torque.z = wrench(2);
 
-        srv[0].request.body_name = "object::object";
-        // srv[0].request.reference_frame = "object::object";
-        // srv.request.reference_point = value3;
-        srv[0].request.wrench.force.x = wrench(0);
-        srv[0].request.start_time = ros::Time(0.0);
-        srv[0].request.duration = ros::Duration(-1.0);
-
-        srv[1].request.body_name = "object::dummy";
-        // srv[1].request.reference_frame = "object::dummy";
-        // srv.request.reference_point = value3;
-        srv[1].request.wrench.force.y = wrench(1);
-        srv[1].request.start_time = ros::Time(0.0);
-        srv[1].request.duration = ros::Duration(-1.0);
-
-        srv[2].request.body_name = "object::dummy_2";
-        // srv[2].request.reference_frame = "object::dummy_2";
-        // srv.request.reference_point = value3;
-        srv[2].request.wrench.torque.z = wrench(2);
-        srv[2].request.start_time = ros::Time(0.0);
-        srv[2].request.duration = ros::Duration(-1.0);
-
-        for (int i = 0; i < 3; i++)
-        {
-            // Call the service
-            if (!client.call(srv[i]))
-            {
-                ROS_ERROR("Failed to call service");
-            }
-        }
-        
-        ROS_INFO("Wrench: %f, %f, %f", wrench(0), wrench(1), wrench(2));
+        pub.publish(wrench_msg);
         
         ros::Duration(0.01).sleep();
     };
