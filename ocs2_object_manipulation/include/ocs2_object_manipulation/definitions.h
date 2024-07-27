b56d9242f8442e0bd554cebe3d88659c86dcf89d
Mohsen <somboles@usc.edu>
2024-07-26 19:13:44 -0700

    from right of obstacles

--- ocs2_object_manipulation/include/ocs2_object_manipulation/definitions.h
+++ ocs2_object_manipulation/include/ocs2_object_manipulation/definitions.h
@@ -8,7 +8,6 @@ namespace object_manipulation {
 constexpr size_t STATE_DIM = 6;
 constexpr size_t INPUT_DIM = 4; //  2 * AGENT_COUNT
 constexpr size_t AGENT_COUNT = 2;
-constexpr size_t OBSTACLE_COUNT = 2;
 
 }  // namespace object_manipulation
 }  // namespace ocs2
